929fda0d43964c402786998a05ca7ad3f87728fe
kartikkanodia <67541628+kartikkanodia@users.noreply.github.com>
2024-06-15 18:34:00 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -5,7 +5,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Step 2: Install required dependencies
-pip install pandas sqlite3 pytest
+pip install -r requirements.txt
 
 # Step 3: Run the Python script
 pytest test_data_pipeline.py
